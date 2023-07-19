a0854f6f6e795b44305e1c95f44e5a2556c3f931
Masana <masanam.rikhotso@gmail.com>
2023-07-19 11:52:54 +0200

    finished ch 22 MERN

--- 5_Web_Dev_Projects/Beginning_MERN/movie-reviews/frontend/src/components/movie.js
+++ 5_Web_Dev_Projects/Beginning_MERN/movie-reviews/frontend/src/components/movie.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import MovieDataService from '../services/movies'
+
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import Media from 'react-bootstrap/Media'
+
+import MovieDataService from "../services/movies"
 import { Link } from 'react-router-dom'
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import Media from 'react-bootstrap/Media';
+import moment from 'moment'
+
 
 const Movie = props => {
     const [movie, setMovie] = useState({
@@ -29,6 +33,30 @@ const Movie = props => {
     useEffect(() => {
         getMovie(props.match.params.id)
     }, [props.match.params.id])
+
+    const deleteReview = (reviewId, index) => {
+        MovieDataService.deleteReview(reviewId, props.user.id)
+            .then(response => {
+                setMovie((currState) => {
+                    currState.reviews.splice(index, 1)
+                    return ({
+                        ...currState
+                    })
+                })
+            })
+            .then(response => {
+                setMovie((prevState) => {
+                    prevState.reviews.splice(index, 1)
+                    return ({
+                        ...prevState
+                    })
+                })
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
     return (
         <div>
             <Container>
@@ -46,11 +74,45 @@ const Movie = props => {
                                 {props.user &&
                                     <Link to={"/movies/" + props.match.params.id + "/review"}>
                                         Add Review
-                                    </Link>}
+                                    </Link>
+                                }
                             </Card.Body>
                         </Card>
                         <br></br>
                         <h2>Reviews</h2>
+                        <br></br>
+                        {/* We access the reviews array and using map, for each review, 
+                            render a Media component from Reactbootstrap */}
+                        {movie.reviews.map((review, index) => {
+                            return (
+                                <Media key={index}>
+                                    <Media.Body>
+                                        <h5>{review.name + " reviewed on " + moment(review.date).format("Do MMMM YYYY")}</h5>
+                                        <p>{review.review}</p>
+                                        {/* A user can only delete reviews they have posted. 
+                                            They can’t delete/edit other’s reviews. 
+                                            Thus, we first  check to see if a user is logged in. 
+                                            And only if the logged in user id is the same as
+                                            the review user id, do we render the Edit/Delete buttons*/}
+                                        {props.user && props.user.id === review.user_id &&
+                                            <Row>
+                                                <Col><Link to={{
+                                                    pathname: "/movies/" +
+                                                        props.match.params.id +
+                                                        "/review",
+                                                    state: { currentReview: review }
+                                                }}>Edit</Link>
+                                                </Col>
+                                                <Col><Button variant="link" onClick={() => deleteReview(review._id, index)}>
+                                                    Delete
+                                                </Button>
+                                                </Col>
+                                            </Row>
+                                        }
+                                    </Media.Body>
+                                </Media>
+                            )
+                        })}
                     </Col>
                 </Row>
             </Container>
