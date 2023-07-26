0d625039feb053db83e04796414254c7b282702a
Masana <masanam.rikhotso@gmail.com>
2023-07-26 12:09:31 +0200

    restarted my ecommerce

---
+++ 5_Web_Dev_Projects/my_ecommerce_app/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    h60
+`
+
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
