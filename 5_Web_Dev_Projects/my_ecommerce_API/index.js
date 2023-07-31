b42bc22a27e96c3b26a5d8522bc183309cea9c89
Masana <masanam.rikhotso@gmail.com>
2023-07-31 05:37:45 +0200

    initialised the api

---
+++ 5_Web_Dev_Projects/my_ecommerce_API/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+
+dotenv.config();
+
+mongoose.
+    connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Successful"))
+    .catch((err) => {
+        console.log("error");
+    });
+
+// This is what you would use if no routes are defined
+// app.get("/api/test", ()=>{
+//     console.log("test is successful")
+// })
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running!");
+});
---
+++ 5_Web_Dev_Projects/my_ecommerce_GUI/src/App.jsx
@@ -0,0 +1,44 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
+import './App.css';
+
+function App() {
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+
+        <Route path="/cart">
+          <Cart />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
