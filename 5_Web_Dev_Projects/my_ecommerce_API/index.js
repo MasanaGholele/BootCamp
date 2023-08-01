670d8de7fae0a5c613e80415cdc8b82898df49c3
Masana <masanam.rikhotso@gmail.com>
2023-08-02 01:28:37 +0200

    routes done

--- 5_Web_Dev_Projects/my_ecommerce_API/index.js
+++ 5_Web_Dev_Projects/my_ecommerce_API/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -19,7 +20,9 @@ mongoose.
 // })
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
