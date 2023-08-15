b80212473518b3371c940795573183458f7b4e57
Masana <masanam.rikhotso@gmail.com>
2023-08-15 16:21:17 +0200

    ecomm API completed

--- 5_Web_Dev_Projects/my_ecommerce_API/routes/user.js
+++ 5_Web_Dev_Projects/my_ecommerce_API/routes/user.js
@@ -37,7 +37,7 @@ router.delete("/:id", verifyTokenAndAuthorisation, async (req, res,) => {
     }
 })
 
-// GET
+// GET A USER BY ADMIN
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
@@ -45,6 +45,47 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
         res.status(200).json(others);
     } catch (err) {
         res.status(500).json(err)
+    }
+})
+
+// GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new
+    try {
+        const users = query
+            ? await User.find().sort({ _id: -1 }).limit(5)
+            : await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET USER STATS BY ADMIN
+router.get("/stats", verifyTokenAndAdmin, async (req, res)=>{
+   const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try{
+
+const data = await User.aggregate([
+    {$match: {createdAt: {$gte: lastYear }}},
+    {
+        $project: {
+            month: { $month: "$createdAt" },
+        },
+    },
+    {
+        $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+        }
+    }
+]);
+res.status(200).json(data)
+
+    }catch(err){
+        res.status(500).json(err);
     }
 })
 
