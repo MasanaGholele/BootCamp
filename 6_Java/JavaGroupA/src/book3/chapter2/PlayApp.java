b09e0b562bfc517a5c0d6fca48de060668df6f00
Masana <masanam.rikhotso@gmail.com>
2023-08-23 10:26:08 +0200

    book 3 ch 3 done

--- 6_Java/JavaGroupA/src/book3/chapter2/PlayApp.java
+++ 6_Java/JavaGroupA/src/book3/chapter2/PlayApp.java
@@ -3,7 +3,7 @@ package book3.chapter2;
 public class PlayApp {
     public static void main(String[] args) {
         Player p1 = new Player();
-        p1.setHealth(10);
+        p1.setHealth(12);
         System.out.println("Player 1 has a health status of " + p1.getHealth());
 
         Player p2 = new Player();
--- 6_Java/JavaGroupA/src/book3/chapter3/CountTestApp.java
+++ 6_Java/JavaGroupA/src/book3/chapter3/CountTestApp.java
@@ -1,19 +1,19 @@
 package book3.chapter3;
 
-public class CountTestApp {
+public class CountTestApp { // keeps track of how many times its constructor has been called
     public static void main(String[] args)
     {
         printCount();
         for (int i = 0; i < 10; i++)
         {
-            CountTest c1 = new CountTest();
-            printCount();
+            CountTest c1 = new CountTest(); // creates an instance of the CountTest class (loops 10 times)
+            printCount(); // printCount method, which prints the number of CountTest objects that have been created so far
         }
     }
     private static void printCount()
     {
         System.out.println("There are now "
-                + CountTest.getInstanceCount()
+                + CountTest.getInstanceCount() //
                 + " instances of the CountTest class.");
     }
 }
