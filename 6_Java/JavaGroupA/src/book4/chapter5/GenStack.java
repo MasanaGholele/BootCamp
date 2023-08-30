cdffe526a5cadff70c77affd81e97a17b7988dd8
Masana <masanam.rikhotso@gmail.com>
2023-08-30 13:05:19 +0200

    book 4 ch 5.1 done

---
+++ 6_Java/JavaGroupA/src/book4/chapter5/GenStack.java
@@ -0,0 +1,44 @@
+package book4.chapter5;
+
+import java.util.*;
+
+public class GenStack<E> {
+    private LinkedList<E> list = new LinkedList<E>();
+
+    public void push(E item) {
+        list.addFirst(item);
+    }
+
+    public E pop() {
+        return list.poll();
+    }
+
+    public E peek() {
+        return list.peek();
+    }
+
+    public boolean hasItems() {
+        return !list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
+class GenStackTest {
+    public static void main(String[] args) {
+        GenStack<String> gs = new GenStack<>();
+        System.out.println("Pushing four items onto the stack");
+        gs.push("One");
+        gs.push("Two");
+        gs.push("Three");
+        gs.push("Four");
+        System.out.println("There are " + gs.size() + " items on the stack");
+        System.out.println("The top item is " + gs.peek() + "\n");
+        System.out.println("Popping everything off the stack! ");
+        while(gs.hasItems()) {
+            System.out.println(gs.pop());
+        }
+        System.out.println("There are " + gs.size() + " items on the stack");
+    }
+}
